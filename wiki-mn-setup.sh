a1e866b7b4c2e5df31ed02d7f9b43d651f62ce38
WikiCoin <44753202+Altcoinwiki@users.noreply.github.com>
2018-11-08 13:50:33 -0500

    Add files via upload

--- wiki-mn-setup.sh
+++ wiki-mn-setup.sh
@@ -1,347 +1,234 @@
-#!/bin/bash
-# Wiki masternode Setup Script V0.1 for Ubuntu 16.04.05 LTS
-# (c) 2018 by WIKIDEV for Wiki Coin
-#
-# Script will attempt to autodetect primary public IP address
-# and generate masternode private key unless specified in command line
-#
-# Usage:
-# bash wiki-mn-setup.sh 
-#
-
-#Color codes
-RED='\033[0;91m'
-GREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
-
-#Wiki TCP port
-PORT=13888
-#Function detect_ubuntu
- if [[ $(lsb_release -d) == *16.04* ]]; then
-   UBUNTU_VERSION=16
-else
-   echo -e "${RED}You are not running Ubuntu 16.04, Installation is cancelled.${NC}"
-   exit 1
-fi
-
-#Clear keyboard input buffer
-function clear_stdin { while read -r -t 0; do read -r; done; }
-
-#Delay script execution for N seconds
-function delay { echo -e "${GREEN}Sleep for $1 seconds...${NC}"; sleep "$1"; }
-
-#Stop daemon if it's already running
-function stop_daemon {
-    if pgrep -x 'wikid' > /dev/null; then
-        echo -e "${YELLOW}Attempting to stop wikid${NC}"
-        wiki-cli stop
-        delay 30
-        if pgrep -x 'wikid' > /dev/null; then
-            echo -e "${RED}wikid daemon is still running!${NC} \a"
-            echo -e "${YELLOW}Attempting to kill...${NC}"
-            pkill wikid
-            delay 30
-            if pgrep -x 'wikid' > /dev/null; then
-                echo -e "${RED}Can't stop wikid! Reboot and try again...${NC} \a"
-                exit 2
-            fi
-        fi
-    fi
+# WIKIcoin masternode install script
+# Edited by Altcoin Cash
+# Edited by MAXDEV
+VERSION="1.0.0.0"
+NODEPORT='13888'
+RPCPORT='3322'
+# Useful variables
+declare -r DATE_STAMP="$(date +%y-%m-%d-%s)"
+declare -r SCRIPT_LOGFILE="/root/.wikicore/log_inst_wiki_node_${DATE_STAMP}_out.log"
+declare -r SCRIPTPATH=$( cd $(dirname ${BASH_SOURCE[0]}) > /dev/null; pwd -P )
+declare -r WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+function print_greeting() {
+	echo -e "[0;35m wiki masternode install script[0m\n"
 }
 
-#Process command line parameters
-genkey=$1
 
-clear
-echo -e "${YELLOW}Wiki masternode Setup Script V1.7 for Ubuntu 16.04 LTS${NC}"
-echo "Do you want me to generate a masternode private key for you? [y/n]"
-  read DOSETUP
-if [[ $DOSETUP =~ "n" ]] ; then
-          read -e -p "Enter your private key:" genkey;
-              read -e -p "Confirm your private key: " genkey2;
-fi
-
-#Confirming match
-  if [ $genkey = $genkey2 ]; then
-     echo -e "${GREEN}MATCH! ${NC} \a" 
-else 
-     echo -e "${RED} Error: Private keys do not match. Try again or let me generate one for you...${NC} \a";exit 1
-fi
-sleep .5
-clear
-
-# Determine primary public IP address
-dpkg -s dnsutils 2>/dev/null >/dev/null || sudo apt-get -y install dnsutils
-publicip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-
-if [ -n "$publicip" ]; then
-    echo -e "${YELLOW}IP Address detected:" $publicip ${NC}
-else
-    echo -e "${RED}ERROR: Public IP Address was not detected!${NC} \a"
-    clear_stdin
-    read -e -p "Enter VPS Public IP Address: " publicip
-    if [ -z "$publicip" ]; then
-        echo -e "${RED}ERROR: Public IP Address must be provided. Try again...${NC} \a"
-        exit 1
-    fi
-fi
-#kill Daemon
-cd ~
-pkill ./wikid
-pkill wikid
-cd Scripts
-
-#Check Deps
-if [ -d "/var/lib/fail2ban/" ]; 
-then
-    echo -e "${GREEN}Dependencies already installed...${NC}"
-else
-    echo -e "${GREEN}Updating system and installing required packages...${NC}"
-
-sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
-sudo apt-get -y autoremove
-sudo apt-get -y install wget nano htop jq
-sudo apt-get -y install libzmq3-dev
-sudo apt-get -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
-sudo apt-get -y install libevent-dev
-sudo apt -y install software-properties-common
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
-sudo apt-get -y update
-sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
-sudo apt-get install unzip
-sudo apt-get -y install libminiupnpc-dev
-sudo apt-get -y install fail2ban
-sudo service fail2ban restart
-sudo apt-get install libdb5.3++-dev libdb++-dev libdb5.3-dev libdb-dev && ldconfig
-sudo apt-get install -y unzip libzmq3-dev build-essential libssl-dev libboost-all-dev libqrencode-dev libminiupnpc-dev libboost-system1.58.0 libboost1.58-all-dev libdb4.8++ libdb4.8 libdb4.8-dev libdb4.8++-dev libevent-pthreads-2.0-5
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install gcc-4.9
-sudo apt-get upgrade libstdc++6
-   fi
-
-#Network Settings
-echo -e "${GREEN}Installing Network Settings...${NC}"
-{
-sudo apt-get install ufw -y
-} &> /dev/null
-echo -ne '[##                 ]  (10%)\r'
-{
-sudo apt-get update -y
-} &> /dev/null
-echo -ne '[######             ] (30%)\r'
-{
-sudo ufw default deny incoming
-} &> /dev/null
-echo -ne '[#########          ] (50%)\r'
-{
-sudo ufw default allow outgoing
-sudo ufw allow ssh
-} &> /dev/null
-echo -ne '[###########        ] (60%)\r'
-{
-sudo ufw allow $PORT/tcp
-sudo ufw allow $RPC/tcp
-} &> /dev/null
-echo -ne '[###############    ] (80%)\r'
-{
-sudo ufw allow 22/tcp
-sudo ufw limit 22/tcp
-} &> /dev/null
-echo -ne '[#################  ] (90%)\r'
-{
-echo -e "${YELLOW}"
-sudo ufw --force enable
-echo -e "${NC}"
-} &> /dev/null
-echo -ne '[###################] (100%)\n'
-
-#Generating Random Password for wikid JSON RPC
-rpcuser=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-rpcpassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-
-#Create 2GB swap file
-#if grep -q "SwapTotal" /proc/meminfo; then
-if grep -q "swapfile" /etc/fstab; then
-    echo -e "${GREEN}Skipping disk swap configuration...${NC} \n"
-else
-    echo -e "${YELLOW}Creating 2GB disk swap file. \nThis may take a few minutes!${NC} \a"
-    touch /var/swap.img
-    chmod 600 /var/swap.img
-    dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-    mkswap /var/swap.img 2> /dev/null
-    swapon /var/swap.img 2> /dev/null
-    if [ $? -eq 0 ]; then
-        echo '/var/swap.img none swap sw 0 0' >> /etc/fstab
-        echo -e "${GREEN}Swap was created successfully!${NC} \n"
-    else
-        echo -e "${RED}Operation not permitted! Optional swap was not created.${NC} \a"
-        rm /var/swap.img
-    fi
-fi
-
-#Installing Daemon
-cd ~
-mkdir ~/Scripts/wiki
-cd Scripts/wiki
-wget https://github.com/Altcoinwiki/source/releases/download/v1.0.0/WikiCoin-1.0.0-x86_64-pc-linux-gnu.zip
-unzip WikiCoin-1.0.0-x86_64-pc-linux-gnu.zip
-rm -r WikiCoin-1.0.0-x86_64-pc-linux-gnu.zip
-stop_daemon
-
-# Deploy binaries to /usr/bin
-sudo cp Scripts/wiki/wiki* /usr/bin/
-sudo chmod 755 -R ~/Scripts
-sudo chmod 755 /usr/bin/wiki*
+function print_info() {
+	echo -e "[0;35m Install script version:[0m ${VERSION}"
+	echo -e "[0;35m Your ip:[0m ${WANIP}"
+	echo -e "[0;35m Masternode port:[0m ${NODEPORT}"
+	echo -e "[0;35m RPC port:[0m ${RPCPORT}"
+	echo -e "[0;35m Date:[0m ${DATE_STAMP}"
+	echo -e "[0;35m Logfile:[0m ${SCRIPT_LOGFILE}"
+}
 
 
-#Create datadir
-if [ ! -f ~/.wikicore/wiki.conf ]; then 
-	sudo mkdir ~/.wikicore
-   cd ~/.wikicore
-   cd ~
-	
-fi
+function install_packages() {
+	echo "Install packages..."
+	add-apt-repository -yu ppa:bitcoin/bitcoin  &>> ${SCRIPT_LOGFILE}
+	apt-get -y update &>> ${SCRIPT_LOGFILE}
+  	apt-get -y upgrade &>> ${SCRIPT_LOGFILE}
+ 	sudo apt-get install p7zip-full &>> ${SCRIPT_LOGFILE}
+	apt-get -y install wget make automake autoconf build-essential libtool autotools-dev \
+	git nano python-virtualenv pwgen virtualenv \
+	pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common \
+	libboost-all-dev libminiupnpc-dev libdb4.8-dev libdb4.8++-dev &>> ${SCRIPT_LOGFILE}
+	sudo add-apt-repository ppa:ubuntu-toolchain-r/test &>> ${SCRIPT_LOGFILE}
+	sudo apt-get update &>> ${SCRIPT_LOGFILE}
+	sudo apt-get install gcc-4.9 &>> ${SCRIPT_LOGFILE}
+	sudo apt-get upgrade libstdc++6 &>> ${SCRIPT_LOGFILE}
+	echo "Install done..."
+}
 
-echo -e "${YELLOW}Creating wiki.conf...${NC}"
 
-# If genkey was not supplied in command line, we will generate private key on the fly
-if [ -z $genkey ]; then
-    cat <<EOF > ~/.wikicore/wiki.conf
-rpcuser=$rpcuser
-rpcpassword=$rpcpassword
-EOF
+function swaphack() {
+	echo "Setting up disk swap..."
+	free -h
+	rm -f /var/wiki_node_swap.img
+	touch /var/wiki_node_swap.img
+	dd if=/dev/zero of=/var/wiki_node_swap.img bs=1024k count=2000 &>> ${SCRIPT_LOGFILE}
+	chmod 0600 /var/wiki_node_swap.img
+	mkswap /var/wiki_node_swap.img &>> ${SCRIPT_LOGFILE}
+	free -h
+	echo "Swap setup complete..."
+}
 
-    sudo chmod 755 -R ~/.wikicore/wiki.conf
- 
-    #Starting daemon first time just to generate masternode private key
-    wikid --daemon
-    sleep 30
 
-    #Generate masternode private key
-    echo -e "${YELLOW}Generating masternode private key...${NC}"
-    genkey=$(wiki-cli masternode genkey)
-    if [ -z "$genkey" ]; then
-        echo -e "${RED}ERROR: Can not generate masternode private key.${NC} \a"
-        echo -e "${RED}ERROR: Reboot VPS and try again or supply existing genkey as a parameter.${NC}"
-        exit 1
-    fi
-    
-    #Stopping daemon to create wiki.conf
-    stop_daemon
-    delay 30
-fi
+function remove_old_files() {
+	echo "Removing old files..."
+	sudo killall wikid
+	sudo rm -rf /root/wiki
+	sudo rm -rf /root/.wikicore
+    	sudo rm -rf /usr/local/bin/wiki*
+	echo "Done..."
+}
 
-# Create wiki.conf
-cat <<EOF > ~/.wikicore/wiki.conf
-rpcuser=$rpcuser
-rpcpassword=$rpcpassword
-rpcallowip=127.0.0.1
-listen=1
-server=1
-daemon=1
-logtimestamps=1
-maxconnections=64
-txindex=1
-masternode=1
-externalip=$publicip:$PORT
-masternodeprivkey=$genkey
-addnode=159.89.25.4:13888
-addnode=178.62.87.144:13888
-addnode=104.248.62.29:13888
-EOF
 
-#Finally, starting Wiki daemon with new wiki.conf
-cd ~
-wikid --daemon
-delay 5
+function download_wallet() {
+	echo "Downloading wallet..."
+	mkdir /root/wiki
+	mkdir /root/.wikicore
+    	cd wiki
+	wget https://github.com/Altcoinwiki/source/releases/download/v1.0.0/WikiCoin-1.0.0-x86_64-pc-linux-gnu.zip 
+	7z x WikiCoin-1.0.0-x86_64-pc-linux-gnu.zip
+	rm /root/wiki/WikiCoin-1.0.0-x86_64-pc-linux-gnu/wiki-qt
+	chmod +x /root/wiki/WikiCoin-1.0.0-x86_64-pc-linux-gnu/*
+	mv /root/wiki/WikiCoin-1.0.0-x86_64-pc-linux-gnu/* /usr/local/bin/
+	rm -rf /root/wiki/
+	echo "Done..."
+}
 
-#Setting auto star cron job for daemon
-cronjob="@reboot sleep 30 && wikid --daemon"
+
+function configure_firewall() {
+	echo "Configuring firewall rules..."
+	apt-get -y install ufw			&>> ${SCRIPT_LOGFILE}
+	# disallow everything except ssh and masternode inbound ports
+	ufw default deny			&>> ${SCRIPT_LOGFILE}
+	ufw logging on				&>> ${SCRIPT_LOGFILE}
+	ufw allow ssh/tcp			&>> ${SCRIPT_LOGFILE}
+	ufw allow 13888/tcp			&>> ${SCRIPT_LOGFILE}
+	ufw allow 3322/tcp			&>> ${SCRIPT_LOGFILE}
+	# This will only allow 6 connections every 30 seconds from the same IP address.
+	ufw limit OpenSSH			&>> ${SCRIPT_LOGFILE}
+	ufw --force enable			&>> ${SCRIPT_LOGFILE}
+	echo "Done..."
+}
+
+
+function configure_masternode() {
+	echo "Configuring masternode..."
+	conffile=/root/.wikicore/wiki.conf
+	PASSWORD=`pwgen -1 20 -n` &>> ${SCRIPT_LOGFILE}
+	if [ "x$PASSWORD" = "x" ]; then
+	    PASSWORD=${WANIP}-`date +%s`
+	fi
+	echo "Loading and syncing wallet..."
+	echo "    if you see *error: Could not locate RPC credentials* message, do not worry"
+	wiki-cli stop
+	echo "It's okay."
+	sleep 10
+	echo -e "rpcuser=wikiuser\nrpcpassword=${PASSWORD}\nrpcport=${RPCPORT}\nrpcallowip=127.0.0.1\nport=${NODEPORT}\nexternalip=${WANIP}\nlisten=1\nmaxconnections=250" >> ${conffile}
+	echo ""
+	echo -e "[0;35m==================================================================[0m"
+	echo -e "     DO NOT CLOSE THIS WINDOW OR TRY TO FINISH THIS PROCESS"
+	echo -e "                        PLEASE WAIT 2 MINUTES"
+	echo -e "[0;35m==================================================================[0m"
+	echo ""
+	wikid -daemon
+	echo "2 MINUTES LEFT"
+	sleep 60
+	echo "1 MINUTE LEFT"
+	sleep 60
+	masternodekey=$(wiki-cli masternode genkey)
+	wiki-cli stop
+	sleep 20
+	echo "Creating masternode config..."
+	echo -e "daemon=1\nmasternode=1\nmasternodeprivkey=$masternodekey" >> ${conffile}
+	echo "Done...Starting daemon..."
+	wikid -daemon
+}
+
+function addnodes() {
+	echo "Adding nodes..."
+	conffile=/root/.wikicore/wiki.conf
+	echo -e "\addnode=159.89.25.4" >> ${conffile}
+	echo -e "addnode=178.62.87.144" >> ${conffile}
+	echo -e "addnode=104.248.62.29" >> ${conffile}
+	echo "Done..."
+}
+
+
+function show_result() {
+	echo ""
+	echo -e "[0;35m==================================================================[0m"
+	echo "DATE: ${DATE_STAMP}"
+	echo "LOG: ${SCRIPT_LOGFILE}"
+	echo "rpcuser=wikiuser"
+	echo "rpcpassword=${PASSWORD}"
+	echo ""
+	echo -e "[0;35m INSTALLED WITH VPS IP: ${WANIP}:${NODEPORT} [0m"
+	echo -e "[0;35m INSTALLED WITH MASTERNODE PRIVATE GENKEY: ${masternodekey} [0m"
+	echo "[0;35m Copy to local Masternode.conf: ${WANIP}:${NODEPORT} ${masternodekey} [0m"
+	echo -e "If you get \"Masternode not in masternode list\" status, don't worry,\nyou just have to start your MN from your local wallet and the status will change"
+	echo -e "[0;35m==================================================================[0m"
+	echo -e "[0;35mCheck your node with command: wiki-cli masternode status[0m"
+	echo -e "[0;35mStop your node with command: wiki-cli stop[0m"
+	echo -e "[0;35mStart your node with command: wikid[0m"
+	echo -e "[0;35m==================================================================[0m"
+}
+
+
+function cleanup() {
+	echo "Cleanup..."
+	apt-get -y autoremove 	&>> ${SCRIPT_LOGFILE}
+	apt-get -y autoclean 		&>> ${SCRIPT_LOGFILE}
+	echo "Done..."
+}
+
+
+#Setting auto start cron job for wikid
+cronjob="@reboot sleep 30 && wikid"
 crontab -l > tempcron
 if ! grep -q "$cronjob" tempcron; then
-    echo -e "${GREEN}Configuring crontab job...${NC}"
+    echo -e "Configuring crontab job..."
     echo $cronjob >> tempcron
     crontab tempcron
 fi
 rm tempcron
 
-echo -e "========================================================================
-${YELLOW}masternode setup is complete!${NC}
-========================================================================
-Masternode was installed with VPS IP Address: ${YELLOW}$publicip${NC}
-Masternode Private Key: ${YELLOW}$genkey${NC}
-Now you can add the following string to the masternode.conf file
-for your Hot Wallet (the wallet with your Wiki collateral funds):
-======================================================================== \a"
-echo -e "${YELLOW}mn1 $publicip:$PORT $genkey TxId TxIdx${NC}"
-echo -e "========================================================================
-Use your mouse to copy the whole string above into the clipboard by
-tripple-click + single-click (Dont use Ctrl-C) and then paste it 
-into your ${YELLOW}masternode.conf${NC} file and replace:
-    ${YELLOW}mn1${NC} - with your desired masternode name (alias)
-    ${YELLOW}TxId${NC} - with Transaction Id from masternode outputs
-    ${YELLOW}TxIdx${NC} - with Transaction Index (0 or 1)
-     Remember to save the masternode.conf and restart the wallet!
-To introduce your new masternode to the Wiki network, you need to
-issue a masternode start command from your wallet, which proves that
-the collateral for this node is secured."
 
-clear_stdin
-read -p "*** Press any key to continue ***" -n1 -s
+# Flags
+compile=0;
+swap=0;
+firewall=0;
 
-echo -e "1) Wait for the node wallet on this VPS to sync with the other nodes
-on the network. Eventually the 'IsSynced' status will change
-to 'true', which will indicate a comlete sync, although it may take
-from several minutes to several hours depending on the network state.
-Your initial Masternode Status may read:
-    ${YELLOW}Node just started, not yet activated${NC} or
-    ${YELLOW}Node  is not in masternode list${NC}, which is normal and expected.
-2) Wait at least until 'IsBlockchainSynced' status becomes 'true'.
-At this point you can go to your wallet and issue a start
-command by either using Debug Console:
-    Tools->Debug Console-> enter: ${YELLOW}masternode start-alias mn1${NC}
-    where ${YELLOW}vn1${NC} is the name of your masternodenode (alias)
-    as it was entered in the masternode.conf file
-    
-or by using wallet GUI:
-    masternodes -> Select masternode -> RightClick -> ${YELLOW}start alias${NC}
-Once completed step (2), return to this VPS console and wait for the
-masternode Status to change to: 'masternode successfully started'.
-This will indicate that your masternode is fully functional and
-you can celebrate this achievement!
-Currently your vrnode is syncing with the Wiki network...
-The following screen will display in real-time
-the list of peer connections, the status of your masternode,
-node synchronization status and additional network and node stats.
-"
-clear_stdin
-read -p "*** Press any key to continue ***" -n1 -s
 
-echo -e "
-${GREEN}...scroll up to see previous screens...${NC}
-Here are some useful commands and tools for masternode troubleshooting:
-========================================================================
-To view masternode configuration produced by this script in wiki.conf:
-${YELLOW}cat ~/.wikicore/wiki.conf${NC}
-Here is your wiki.conf generated by this script:
--------------------------------------------------${YELLOW}"
-cat ~/.wikicore/wiki.conf
-echo -e "${NC}-------------------------------------------------
-NOTE: To edit wiki.conf, first stop the Wikicoiond daemon,
-then edit the wiki.conf file and save it in nano: (Ctrl-X + Y + Enter),
-then start the wikid daemon back up:
-to stop:   ${YELLOW}./wiki-cli stop${NC}
-to edit:   ${YELLOW}nano ~/.wikicore/wiki.conf${NC}
-to start:  ${YELLOW}./wikid${NC}
-========================================================================
-To view wikid debug log showing all MN network activity in realtime:
-${YELLOW}tail -f ~/.wikicore/debug.log${NC}
-========================================================================
+#Bad arguments
+if [ $? -ne 0 ];
+then
+    exit 1
+fi
 
-"
 
-# EOF
+# Check arguments
+while [ "$1" != "" ]; do
+    case $1 in
+        -sw | --swap )
+            swap=1
+            ;;
+        -f | --firewall )
+            firewall=1
+            ;;
+        -n | --addnodes )
+            addnodes=1
+            ;;
+        * )
+            exit 1
+    esac
+    if [ "$#" -gt 0 ]; then shift; fi
+done
+
+
+# main routine
+print_greeting
+print_info
+install_packages
+if [ "$swap" -eq 1 ]; then
+	swaphack
+fi
+
+if [ "$firewall" -eq 1 ]; then
+	configure_firewall
+fi
+
+remove_old_files
+download_wallet
+addnodes
+configure_masternode
+
+show_result
+cleanup
+echo "All done!"
+cd ~/
+sudo rm /root/wiki_ubuntu_install.sh
